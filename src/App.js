78c7f1d4eb8b40d73a6daeaa563275e39d940e08
Nazim <khalilov.nazim72@gmail.com>
2021-09-17 17:51:15 +0300

    creating and working with components

--- src/App.js
+++ src/App.js
@@ -1,17 +1,26 @@
-import React, {Component} from 'react'
-import classes from './App.module.scss'
+import React, { Component } from 'react'
+import './App.scss'
 import Car from './Car/Car'
+import ErrorBoundary from './ErrorBoundary/ErrorBoundary'
+import Counter from './Counter/Counter'
+
+export const ClickedContext = React.createContext(false)
 
 class App extends Component {
 
-    state = {
-        cars: [
-            {name: 'Audi', year: 2018},
-            {name: 'BMW', year: 2016},
-            {name: 'Mazda', year: 2010},
-        ],
-        pageTitle: 'React components',
-        showCars: true
+    constructor(props) {
+        super(props)
+
+        this.state = {
+            clicked: false,
+            cars: [
+                {name: 'Ford', year: 2018},
+                {name: 'Audi', year: 2016},
+                {name: 'Mazda', year: 2010}
+            ],
+            pageTitle: 'React components',
+            showCars: true
+        }
     }
 
     toggleCarsHandler = () => {
@@ -31,42 +40,70 @@ class App extends Component {
     deleteHandler(index) {
         const cars = this.state.cars.concat()
         cars.splice(index, 1)
+
         this.setState({cars})
+
     }
 
+    // componentWillMount() {
+    //     console.log('App componentWillMount')
+    // }
+    //
+    // componentDidMount() {
+    //     console.log('App componentDidMount')
+    // }
+
     render() {
+        // console.log('App render')
+        const divStyle = {
+            textAlign: 'center'
+        }
 
         let cars = null
 
         if (this.state.showCars) {
             cars = this.state.cars.map((car, index) => {
-                return <Car
-                    key={index}
-                    name={car.name}
-                    year={car.year}
-                    onDelete={this.deleteHandler.bind(this, index)}
-                    onChangeName={(event) => this.onChangeName(event.target.value, index)}
-                />
+                return (
+                    <ErrorBoundary key={index}>
+                        <Car
+                            name={car.name}
+                            year={car.year}
+                            index={index}
+                            onDelete={this.deleteHandler.bind(this, index)}
+                            onChangeName={event => this.onChangeName(event.target.value, index)}
+                        />
+                    </ErrorBoundary>
+                )
             })
         }
 
         return (
-            <div className={classes.App}>
-                <h1>{this.state.pageTitle}</h1>
+            <div style={divStyle}>
+                {/*<h1>{this.state.pageTitle}</h1>*/}
+                <h1>{this.props.title}</h1>
 
+                <ClickedContext.Provider value={this.state.clicked}>
+                    <Counter />
+                </ClickedContext.Provider>
+
+                <hr/>
                 <button
+                    style={{marginTop: 20}}
+                    className={'AppButton'}
                     onClick={this.toggleCarsHandler}
                 >Toggle cars</button>
+
+                <button onClick={() => this.setState({clicked: !this.state.clicked})}>Change clicked</button>
 
                 <div style={{
                     width: 400,
                     margin: 'auto',
-                    paddingTop: 20
+                    paddingTop: '20px'
                 }}>
                     { cars }
                 </div>
             </div>
-        )
+        );
     }
 }
 
--- src/Car/Car.js
+++ src/Car/Car.js
@@ -1,44 +1,57 @@
 import React from 'react'
-import Radium from 'radium';
+import classes from './Car.module.scss'
+import PropTypes from 'prop-types'
+import withClass from '../hoc/withClass'
 
-import './Car.scss'
+class Car extends React.Component {
 
-const Car = props => {
-    const inputClasses = ['input']
+    constructor(props) {
+        super(props);
 
-    if (props.name !== '') {
-        inputClasses.push('green')
-    } else {
-        inputClasses.push('red')
+        this.inputRef = React.createRef()
     }
 
-    if (props.name.length > 4) {
-        inputClasses.push('bold')
-    }
-
-    const style = {
-        border: '1px solid #ccc',
-        boxShadow: '0 4px 5px 0 rgba(0, 0, 0, .14)',
-        ':hover': {
-            border: '1px solid #aaa',
-            boxShadow: '0 4px 15px 0 rgba(0, 0, 0, .25)',
-            cursor: 'pointer'
+    componentDidMount() {
+        if (this.props.index === 1) {
+            this.inputRef.current.focus()
         }
     }
 
-    return (
-        <div className="Car" style={style}>
-            <h3>Car name: {props.name}</h3>
-            <p>Year: <b>{props.year}</b></p>
-            <input
-                type="text"
-                onChange={props.onChangeName}
-                value={props.name}
-                className={inputClasses.join(' ')}
-            />
-            <button onClick={props.onDelete}>Delete</button>
-        </div>
-    )
+    render() {
+        const inputClasses = [classes.input]
+
+        if (this.props.name !== '') {
+            inputClasses.push(classes.green)
+        } else {
+            inputClasses.push(classes.red)
+        }
+
+        if (this.props.name.length > 4) {
+            inputClasses.push(classes.bold)
+        }
+        return (
+            <React.Fragment>
+                <h3>Сar name: {this.props.name}</h3>
+                <p>Year: <strong>{this.props.year}</strong></p>
+                <input
+                    ref={this.inputRef}
+                    type="text"
+                    onChange={this.props.onChangeName}
+                    value={this.props.name}
+                    className={inputClasses.join(' ')}
+                />
+                <button onClick={this.props.onDelete}>Delete</button>
+            </React.Fragment>
+        )
+    }
 }
 
-export default Radium(Car)
+Car.propTypes = {
+    name: PropTypes.string.isRequired,
+    year: PropTypes.number,
+    index: PropTypes.number,
+    onChangeName: PropTypes.func,
+    onDelete: PropTypes.func
+}
+
+export default withClass(Car, classes.Car)
